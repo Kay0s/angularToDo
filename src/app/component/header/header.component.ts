80fb6af10e1b29a8685ef187fc7fb9ae37d79b8f
Kay0s <hamilton.kristina@gmail.com>
2021-05-16 22:18:35 -0500

    added title element into HeaderComponent export class

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -3,11 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-
-  title = 'angularToDo';
+  title: string = 'AngularToDo';
+  
 
   constructor() { }
 
--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.css']
+  styleUrls: ['./button.component.css'],
 })
 export class ButtonComponent implements OnInit {
+  @Input() text: string;
+  @Input() color: string;
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+  constructor() {}
+
+  ngOnInit(): void {}
+
 
 }
