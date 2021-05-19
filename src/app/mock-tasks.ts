8323702510ad889d7cf6feee97cd7a4f920c8f99
Kay0s <hamilton.kristina@gmail.com>
2021-05-18 20:38:13 -0500

    created task seed

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Appointment',
+    day: 'May 19th at 2:30pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Meeting',
+    day: 'May 26th at 1:30pm',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Shopping',
+    day: 'May 27th at 12:30pm',
+    reminder: false,
+  },
+];
