801e18de323d6f61d5f3ae09c94da482f3b00143
Kay0s <hamilton.kristina@gmail.com>
2021-05-23 23:34:45 -0500

    passed private http:HttpClient as argument in task.service.ts

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -10,7 +10,7 @@ import {TASKS} from '../mock-tasks';
 export class TaskService {
   private apiUrl = 'http://localhost:5000/tasks'
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   getTasks(): Observable<Task[]> {
     const tasks = of(TASKS);
