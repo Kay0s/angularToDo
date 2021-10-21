790f4fa412d7f71825e5c60bd0def76bad1656a2
Kay0s <hamilton.kristina@gmail.com>
2021-10-20 19:52:48 -0500

    export class title

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,12 +3,14 @@ import { UiService } from 'src/app/services/ui.service';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  title: string = 'Task Tracker';
 
   constructor() { }
 
