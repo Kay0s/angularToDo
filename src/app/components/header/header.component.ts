e121dfc4e0d7f43fd7995abf015712210ee2cb47
Kay0s <hamilton.kristina@gmail.com>
2021-10-20 19:51:31 -0500

    imported Subscription

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UiService } from 'src/app/services/ui.service';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
